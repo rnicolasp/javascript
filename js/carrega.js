00d250a4a2bac966bc60a4875375d321b8ec591f
rnicolasp <rnicolasp@esliceu.net>
2023-03-23 20:39:53 +0100

    no se la verdad

--- js/carrega.js
+++ js/carrega.js
@@ -1,26 +1,87 @@
-const nom = document.querySelector("#nom");
-const llin1 = document.querySelector("#llin1");
+const selectRover = document.querySelector("#rover");
+const selectCameras = document.querySelector("#cameras");
+const selectPagines = document.querySelector("#pagina");
+const divResultat = document.querySelector("#resultat");
 const btnCarrega = document.querySelector("#btnCarrega");
+const divLoading = document.querySelector("#loading");
+const fecha = document.querySelector("#data");
+const camerasCuriosity = ['FHAZ', 'RHAZ', 'MAST', 'CHEMCAM', 'MAHLI', 'MARDI', 'NAVCAM'];
+const camerasOpportunitySpirit = ['FHAZ', 'RHAZ', 'NAVCAM', 'PANCAM', 'MINITES'];
 
 btnCarrega.onclick = carrega;
+selectRover.oninput = carregaCameras;
+
+function carregaCameras() {
+    var arrayARecorrer = []
+    if (selectRover.ariaValueMax.toLowerCase() == "Curiosity".toLowerCase) {
+        arrayARecorrer = camerasCuriosity;
+    } else {
+        arrayARecorrer = camerasOpportunitySpirit;
+    }
+
+    var resultat = "<option value=''>Selecciona</option>";
+    arrayARecorrer.forEach(function (element) {
+        resultat += `<option value="${element}">${element}</option>`
+    });
+
+    selectCameras.innerHTML = resultat;
+}
+
+carregaCameras();
 
 function carrega() {
 
-    fetch("/json/dades.json")
+
+
+    var roverSeleccionat = selectRover.value;
+    var URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverSeleccionat}/photos?&api_key=a20QuAMHhKUFO8KrqnV4JNzrlpd9igSpOjWSzvmy`;
+
+    if (selectCameras.value) {
+        URL += `&camera=${selectCameras.value}`;
+    }
+
+    URL += `&page=${selectPagines.value}`;
+
+    if (fecha.value) {
+        URL += `&earth_date=${fecha.value}`;
+        console.log(URL);
+    }
+
+    fetch(URL)
         .then(response => {
-            
-           if (response.ok) return response.json();
-           else {
-            alert("No s'ha pogut completar la càrrega " + response.status);
-           }
+
+            if (response.ok) return response.json();
+            else {
+                alert("No s'ha pogut completar la càrrega " + response.status);
+            }
 
         })
         .then(data => {
-                carregaDades(data);
+            carregaDades(data);
         });
+
+    console.log(URL);
 }
 
-function carregaDades(dades){
-    nom.value = dades.nom;
-    llin1.value = dades.llin1;
+function carregaDades(data) {
+
+    let resultat = "";
+
+    if (data.photos.length) {
+
+        data.photos.forEach((element, index) => {
+
+            let earth_date = element.earth_date;
+            let dataArray = earth_date.split("-");
+            let novaData = `${dataArray[2]}/${dataArray[1]}/${dataArray[0]}`;
+            resultat += `<div class="col-6 col-sm-3"><p>
+            <label>Imatge amb id ${element.id} de dia ${novaData}. Rover name: ${element.rover.name} </label>
+            <img class="img-fluid" src='${element.img_src}'></img>
+            </p></div>`
+        })
+    } else {
+        resultat = "No s'han trobat dades";
+    }
+    divResultat.innerHTML = resultat;
+
 }
